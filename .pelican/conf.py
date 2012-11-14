111f062c44908aa6329684cdd14738fc3a2081f9
Aldrin D'Souza <a1drin@me.com>
2012-11-14 21:48:51 +0530

    adding pelican based generation

---
+++ .pelican/conf.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+MARKUP = 'md'
+THEME = 'theme'
+DEFAULT_LANG = 'en'
+STATIC_PATHS = ['images']
+TIMEZONE = 'Asia/Calcutta'
+SITEURL = u'http://a1dr.in'
+AUTHOR = u"Aldrin J D'Souza"
+SITENAME = u"Aldrin's Notebook"
+TAG_FEED_ATOM = 'tag/%s.atom.xml'
+DEFAULT_DATE_FORMAT = '%b %d, %Y'
+FILES_TO_COPY = (('README.txt', 'README.md'), ('CNAME', 'CNAME'))
+MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
---
+++ README.md
@@ -0,0 +1,4 @@
+Aldrin's Notebook
+=================
+
+This repo contains my web pages - published at http://a1dr.in
